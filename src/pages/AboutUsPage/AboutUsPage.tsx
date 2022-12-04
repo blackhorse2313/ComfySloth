275124a64b3bdbab4b4acd48de8ccad26dacdd26
breadhead313 <kanenorth48@gmail.com>
2022-12-04 23:17:47 +0400

    AboutPageAdded

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -3,6 +3,18 @@ import './AboutUsPage.scss'
 
 export const AboutUsPage = () =>{
   return(
-    <h1>hello this is about us</h1>
+    <section className="our-story container"> 
+              
+                <img src="./assets/img/hero-bcg.a876f19f6786a3aca992.jpeg" alt=""/>
+
+                <article>
+                    <div className="title">
+                        <h2>Our Story</h2>
+                        <div className="underline"></div>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat accusantium sapiente tempora sed dolore esse deserunt eaque excepturi, delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt sequi blanditiis est exercitationem molestiae delectus saepe odio eligendi modi porro eaque in libero minus unde sapiente consectetur architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum totam velit saepe sed quos similique amet. Ex, voluptate accusamus nesciunt totam vitae esse iste.</p>
+                </article>
+    
+    </section>
   )
 }
