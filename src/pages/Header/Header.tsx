d241f566fa2c23865d45706f818e039169de1342
breadhead313 <kanenorth48@gmail.com>
2022-12-05 09:54:19 +0400

    HeaderChanges

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -25,14 +25,6 @@ export const Header = () => {
         </div>
        
       </nav>
-      <article>
-        <div className="title">
-          <h2>Our Story</h2>
-          <div className="underline"></div>
-        </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat accusantium sapiente tempora sed dolore esse deserunt eaque excepturi, delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt sequi blanditiis est exercitationem molestiae delectus saepe odio eligendi modi porro eaque in libero minus unde sapiente consectetur architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum totam velit saepe sed quos similique amet. Ex, voluptate accusamus nesciunt totam vitae esse iste.</p>
-      </article>
-
     </section>
   )
 }
