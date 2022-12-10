85d3409f668db4600aac683f66b7b9ff5b1f5ffd
breadhead313 <kanenorth48@gmail.com>
2022-12-10 10:19:02 +0400

    HomePageSliderUpdate

--- src/pages/HomePage/components/Slider/slider.tsx
+++ src/pages/HomePage/components/Slider/slider.tsx
@@ -1,6 +1,6 @@
 import './Slider.scss';
-import picture from '../../../../assets/images/hero-bcg.a876f19f6786a3aca992.jpeg'
-
+import picture1 from '../../../../assets/images/hero-bcg.a876f19f6786a3aca992.jpeg'
+import picture2 from '../../../../assets/images/slider2.jpeg'
 
 
 export const Slider = () => {
@@ -8,16 +8,17 @@ export const Slider = () => {
         <section id="slider">
             <div className="container">
                 <div className="row">
-                    <div className="col-6">
+                    <div className="col-6 col-md-12">
                         <div className="left">
                             <h1>Design YourComfort Zone</h1>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at sed omnis corporis doloremque possimus velit! Repudiandae nisi odit, aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis alias?</p>
                             <button>Shop Now</button>
                         </div>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 col-md-12">
                         <div className="right">
-                            <img src={picture} alt=""/>
+                            <div className="img1">  <img src={picture1} alt="img" />
+                                <div className="img2"><img src={picture2} alt="img"/></div></div>
                         </div>
                     </div>
                 </div>
