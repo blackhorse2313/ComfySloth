ca99b95b9e2a92f58c51e0a55d697440ef2eb135
breadhead313 <kanenorth48@gmail.com>
2022-12-22 23:44:27 +0400

    Feature

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import './HomePage.scss';
 import { Furniture } from './components/Furniture/Furniture';
 import { JoinUs } from './components/JoinUs/JoinUs';
 import { Slider } from './components/Slider/slider';
+import {FeatureProduct} from './components/FeatureProduct/FeatureProduct';
 
 export const HomePage = () => {
   return (
     <>
      <Slider />
+     <FeatureProduct/>
       <Furniture />
       <JoinUs />
     </>
---
+++ src/pages/HomePage/components/FeatureProduct/FeatureProduct.tsx
@@ -0,0 +1,114 @@
+import './FeatureProduct.scss';
+
+export const FeatureProduct = () => {
+  return (
+    <section className="FeatureProduct">
+      <div className="container">
+        <div className="title">
+          <h2>Featured Products</h2>
+
+          <div className="underline"></div>
+        </div>
+
+        <div className="row featured">
+          <div className="col-4">
+            <div className="hover">
+              <img
+                src="https://v5.airtableusercontent.com/v1/13/13/1671746400000/3Dlf1eqWdqH88EscXHtgTA/MtB326UzKigzKfqSli8Lq5seHdXOOsL6_I96PqdDqUpRTwMGOzITq6zTNE3Jaj-nWLZfieWVHsBDtUVGIvJamw/67v8ZY26WTvG8gW1wGTFbEcsdDgsQfrrk16H-6_DE6s"
+                alt="entertainment center"
+              />
+              <a className="link" href="/">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                </svg>
+              </a>
+            </div>
+
+            <div className="footer">
+                <h5>
+                entertainment center
+                </h5>
+                <p>
+                $599.99
+                </p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="hover">
+              <img
+                src="https://v5.airtableusercontent.com/v1/13/13/1671746400000/3Dlf1eqWdqH88EscXHtgTA/MtB326UzKigzKfqSli8Lq5seHdXOOsL6_I96PqdDqUpRTwMGOzITq6zTNE3Jaj-nWLZfieWVHsBDtUVGIvJamw/67v8ZY26WTvG8gW1wGTFbEcsdDgsQfrrk16H-6_DE6s"
+                alt="entertainment center"
+              />
+              <a className="link" href="/">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                </svg>
+              </a>
+            </div>
+
+            <div className="footer">
+                <h5>
+                entertainment center
+                </h5>
+                <p>
+                $599.99
+                </p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="hover">
+              <img
+                src="https://v5.airtableusercontent.com/v1/13/13/1671746400000/3Dlf1eqWdqH88EscXHtgTA/MtB326UzKigzKfqSli8Lq5seHdXOOsL6_I96PqdDqUpRTwMGOzITq6zTNE3Jaj-nWLZfieWVHsBDtUVGIvJamw/67v8ZY26WTvG8gW1wGTFbEcsdDgsQfrrk16H-6_DE6s"
+                alt="entertainment center"
+              />
+              <a className="link" href="/">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                </svg>
+              </a>
+            </div>
+
+            <div className="footer">
+                <h5>
+                entertainment center
+                </h5>
+                <p>
+                $599.99
+                </p>
+            </div>
+          </div>
+        </div>
+
+       
+
+        <a className='btn' href="/products">all products</a>
+
+
+      </div>
+    </section>
+  );
+};
